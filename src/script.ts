43bb3ec7d6c2a5184a5696c17e1d1639b2c63dce
awef <idawef@gmail.com>
2014-05-10 10:34:39 +0900

    テストを通るよう修正

--- src/script.ts
+++ src/script.ts
@@ -2,6 +2,7 @@
 ///<reference path="../lib/DefinitelyTyped/jquery/jquery.d.ts" />
 ///<reference path="controller/index.ts" />
 ///<reference path="directive/PanelContainer.ts" />
+///<reference path="service/Adapter/ChLikeBBS.ts" />
 
 angular.module("app", ["BBSIndex", "PanelContainer"]);
 
