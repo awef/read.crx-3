f1ee1e2b28aa3847cf4885c6a84399c773c9ec6c
awef <idawef@gmail.com>
2014-05-25 21:58:14 +0900

    スレ立て時刻の表示機能を実装

--- src/controller/board.ts
+++ src/controller/board.ts
@@ -1,17 +1,22 @@
 ///<reference path="../../lib/DefinitelyTyped/angularjs/angular.d.ts" />
 ///<reference path="../../lib/DefinitelyTyped/jquery/jquery.d.ts" />
 ///<reference path="../service/Adapter/AdapterAgent.ts" />
+///<reference path="../Util.ts" />
 
 interface BoardCtrlScope extends ng.IScope {
   url: string;
   message: string;
   boardEntries: App.Board;
   move: Function;
+  dateToString: Function;
 }
 
 class BoardCtrl {
   constructor ($scope: BoardCtrlScope, adapterAgent: App.AdapterService) {
     $scope.message = "取得中"
+    $scope.dateToString = function (date: number) {
+      return App.Util.dateToString(new Date(date));
+    };
 
     adapterAgent.get($scope.url).then(
       (res) => {
